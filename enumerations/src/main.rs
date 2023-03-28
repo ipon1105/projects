aa0b6fa4074c865578fb3eab284cfdf480fc7b4c
Ivan <my_world1105@mail.ru>
2023-03-28 18:56:54 +0300

    lesson18

--- enumerations/src/main.rs
+++ enumerations/src/main.rs
@@ -1,33 +1,25 @@
-#[derive(Debug)]
-enum IpAddrKind {
-    V4(u8, u8, u8, u8),
-    V6(String),
-    V8{x: i32, y: i32},
-}
-
-impl IpAddrKind {
-    fn print(&self) {
-        println!("ip = {:?}", self);
-    }
+enum rub {
+    one,
+    two, 
+    five,
 }
 
 fn main() {
-    // Перечисление (enumeration)
-    // Значение перечислений
-    // Методы для перечислений
-    let ip4: IpAddrKind = IpAddrKind::V4(192, 168, 0, 1);
-    let ip6: IpAddrKind = IpAddrKind::V6(String::from("::1"));
-    let ip8: IpAddrKind = IpAddrKind::V8{x:32, y:32};
+    // match
+    let money = rub::five;
+    match money {
+        rub::one => { println!("one"); },
+        rub::two => println!("two"),
+        rub::five => println!("five"),
+    }
 
-    ip4.print();
-    ip6.print();
-    ip8.print();
-
-    let x: Option<i32> = Some(32);
-    let y: i32 = 15;
+    // Шаблоны со значениями
+    // Универсальный шаблон и заполнить _
+    let x = 7;
+    match x {
+        5 => println!("Ты победил"),
+        10 => println!("Повтори ещё"),
+        other => println!("Выпало {other}. Ты проиграл"),
+    }
     
-    // let z = x + y;
-    // Перечисление Option<T> и почему в Rust нет null
-    // Если Вы попытаетесь использовать null значение в качестве not-null значения,
-    // Вы получите ошибку определённого рода
 }
