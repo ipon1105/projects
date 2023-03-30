7736910c2e6620d2f45d069b7c7f5e11bf01d358
Ivan <my_world1105@mail.ru>
2023-03-30 20:39:07 +0300

    lesson22

--- organization/farm/src/garden/vegetables/mod.rs
+++ organization/farm/src/garden/vegetables/mod.rs
@@ -1,4 +1,27 @@
+pub enum type_of_cost {
+    free,
+    expansive,
+}
+
+#[derive(Debug)]
+pub struct Tomat{
+    count: u8,
+    pub type_of_plant: String,
+}
+
+impl Tomat {
+    pub fn plant(type_of_plant: String) -> Self {
+        Self {
+            count: 3,
+            type_of_plant,
+        }
+    }
+}
+
 pub fn from_vegetables(){
     println!("hello from vegetables");
+}
 
+pub fn call_garden(){
+    super::from_garden();
 }
--- organization/farm/src/main.rs
+++ organization/farm/src/main.rs
@@ -1,9 +1,16 @@
 mod garden;
 
 use garden::from_garden;
-use garden::vegetables::from_vegetables;
+use garden::vegetables::from_vegetables;    
+use garden::vegetables::call_garden;
+use garden::vegetables::Tomat;
+use garden::vegetables::type_of_cost;
 
 fn main() {
     from_garden();
     from_vegetables();
+    call_garden();
+
+    let x = Tomat::plant(String::from("yellow"));
+    println!("x = {:?}",x);
 }
