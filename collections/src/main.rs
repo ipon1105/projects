03caa545b16d2bb68bc04806e3ede84845019aa7
Ivan <my_world1105@mail.ru>
2023-04-02 22:16:48 +0300

    lesson26

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,34 +1,33 @@
 fn main() {
-    // Строки - Изменение
-    let x: String = String::new();
-    let x: String = "string".to_string();
-    let mut x: String = String::from("strings");
-    println!("x = {}", x);
+    // HashMap
+    use std::collections::HashMap;
 
-    // x.push('a');
-    x.push_str(" is cool");
-    println!("x = {}", x);
+    // Создание, Изменение, Владение, Обновление
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Red"), 20);
+    scores.insert(String::from("Red"), 21);
 
-    let a: String = String::from("He你llo");
-    let space: String = String::from(" ");
-    let b: String = String::from("World");
+    let str = "Bluu".to_string();
+    let x = scores.get(&str).copied().unwrap_or(0);
+    println!("x = {x}");
     
-    // let res = a + &space + &b;
-    let res = format!("{a} {b}");
-    println!("a = {}", a);
-    println!("b = {}", b);
-    println!("res = {}", res);
-    // Индексирование
-    // let x = res[0];
-    // Байты, Скалярные значения и Кластеры Графем
-    // Срезы строк
-    // let s = &res[0..4];
-    // println!("s = {}", s);
-    // Перебор
-    for c in "3Д".chars(){
-        println!("c = {c}");
+    for (key, value) in &scores {
+        println!("key({key}) = value({value})");
     }
-    for c in "3 Д".bytes(){
-        println!("byte = {c}");
+
+    let xxx = scores.entry(String::from("Red")).or_insert(20);
+    for (key, value) in &scores {
+        println!("key({key}) = value({value})");
     }
+    
+    let text = "hello world wonderful world";
+    let mut word_map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = word_map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", word_map);
 }
