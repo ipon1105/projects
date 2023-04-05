13cb0abfb33ea08e8f0aa5fe0ebcf32c1c536140
Ivan <my_world1105@mail.ru>
2023-04-05 22:37:53 +0300

    lesson29

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,9 +1,28 @@
 use std::{fs::File, io::{ErrorKind, Read}};
 use std::error::Error;
 
+struct  {
+    value: i32,
+}
+
+impl Guess {
+    fn new(value: i32) -> Self {
+        if value > 0 && value < 101 {
+            return Self{value}
+        } 
+        return Self { value: 3 }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
+    // Когда стоит использовать unwrap и expect
+    // Пользовательский тип для проверки
+
+    let g = 100;
+
     let mut name: String = String::new();
-    File::open("file.txt")?.read_to_string(&mut name)?;
+    let file = File::open("5.txt").unwrap();
+    // File::open("file.txt")?.read_to_string(&mut name)?;
 
     println!("name = {}", name);
     Ok(())
